6060fbd0dac700e967770995d051ef68e6a8cf30
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-20 08:51:58 +0200

    added new function

--- day-1-add/script.js
+++ day-1-add/script.js
@@ -1,1 +1,10 @@
+function addNums(...nums) {  // concatenates all the input numbers into array
+    let sum = 0;
+    nums.forEach((num) => sum += num);
 
+    return sum;
+    // console.log(sum);
+}
+
+// Test case and function call
+addNums(1, 2, 3, 8);
