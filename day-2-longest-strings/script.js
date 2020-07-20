836a774d5241d3d896467189a8e19e9beeef20d0
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-20 09:03:43 +0200

    started solution

--- day-2-longest-strings/script.js
+++ day-2-longest-strings/script.js
@@ -1,1 +1,14 @@
+// Given an array of strings, return another array containing all of its longest strings
 
+function longestString(arr) {
+    // find the longest length
+    let longestLength = 0;
+
+    for(let word of arr) {
+        longestLength = word.length > longestLength ? word.length : longestLength;
+    }
+
+    // return the longest string
+}
+
+longestString("mico", "dennie", "angeli", "apolllo");
