3c45a047618d5dee17849f0f2f8d0596b61614e7
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-20 23:03:07 +0200

    added solution

--- day-3-alternating-sums/script.js
+++ day-3-alternating-sums/script.js
@@ -1,1 +1,19 @@
+// Return an array of sums of even and odd numbers in an array
 
+function alternatingSums(arr) {
+    let evenSum = 0;
+    let oddSum = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(i % 2 === 0) {
+            evenSum += arr[i];
+        } else {
+            oddSum += arr[i];
+        }
+    }
+    console.log ([evenSum, oddSum]);
+    // return [evenSum, oddSum];
+}
+
+const nums = [50, 60, 60, 45, 70];
+alternatingSums(nums);
