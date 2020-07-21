d4fe0f6dfe1f739fbe80248c27e5412d522beb03
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-21 09:12:44 +0200

    added solution

--- day-4-array-replace/script.js
+++ day-4-array-replace/script.js
@@ -1,1 +1,25 @@
+// Given an array of integers, replace all the occurences of elemToReplace with substitutionElem
 
+function arrayToReplace(array, elemToReplace, substitutionElem) {
+    // iterate through list
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === elemToReplace) {
+            array[i] = substitutionElem;
+        }
+    }
+
+    // check for matching number
+
+    // update the match
+
+    // return value
+    return array;
+}
+
+const array = [1, 2, 3];
+const elemToReplace = 1;
+const substitutionElem = 3;
+
+const result = arrayToReplace(array, elemToReplace, substitutionElem);
+
+console.log("result: ", result);
