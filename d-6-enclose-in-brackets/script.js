7f13f0f3876b830f07f8e09ffb06f1572a2587f3
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-22 22:43:15 +0200

    added three solutions

--- d-6-enclose-in-brackets/script.js
+++ d-6-enclose-in-brackets/script.js
@@ -1,1 +1,20 @@
+// Given a string, enclose it in round brackets
 
+function encloseInBrackets(str) {
+    // ES5:
+    const solution1 = '(' + str + ')';
+
+    // ES6
+    const solution2 = `(${str})`;
+
+    // concatenation
+    const solution3 = '('.concat(str, ')');
+
+    return solution1;
+
+}
+
+const str = "beautiful day";
+const result = encloseInBrackets(str);
+
+console.log("result:", result);
