6faf08ddfe4f3a851ea178c57f0030941e27dae6
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-07-22 22:21:15 +0200

    added solution

--- d-5-case-insensitive-palindrome/script.js
+++ d-5-case-insensitive-palindrome/script.js
@@ -1,1 +1,25 @@
+// Given a string, check if it can become a palindrome through a case change of some (possibly, none) letters
 
+/*
+Hints:
+join();
+reverse();
+toLowerCase();
+split();
+*/
+
+function caseInsensitivePalindrome(str) {
+    // make str case-less
+    const caselessStr = str.toLowerCase();
+
+    // compare it forwards and backwards
+    const reversedCaselessStr = caselessStr.split('').reverse().join('');
+
+    // return result
+    return caselessStr === reversedCaselessStr;
+}
+
+const str = "anna";
+const result = caseInsensitivePalindrome(str);
+
+console.log("result:", result);
