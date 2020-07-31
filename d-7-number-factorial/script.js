5538c91ccd4574b7fc582fdcf5887b99765f520e
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-08-01 01:21:14 +0200

    setup for script

--- d-7-number-factorial/script.js
+++ d-7-number-factorial/script.js
@@ -1,1 +1,13 @@
+// Number Factorial
 
+// Return the factorial of the provided integer
+// Factorials are often represented with the shorthand notation!
+// For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+function factorialNumber(num) {
+
+};
+
+const num = 6;
+const result = factorialNumber(num);
+console.log("result: ", result);
