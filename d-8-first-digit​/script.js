0da72ffaca9c913149a18522c33dbef9073058a8
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-08-03 20:46:26 +0200

    solution added

--- d-8-first-digit​/script.js
+++ d-8-first-digit​/script.js
@@ -6,4 +6,21 @@ Hints:
 split()
 includes()
 
+or regular expressions
 */
+
+function firstDigit(str) {
+    const strNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const chars = str.split('');
+
+    for (const char of chars) {
+        if (strNums.includes(char)) {
+            return char;
+        }
+    }
+}
+
+const str = "var_1__Int2";
+const result = firstDigit(str);
+
+console.log("result:", result);
