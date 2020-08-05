6487fd5d017e08aaf8814acee6d52ed719c61181
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-08-05 07:52:16 +0200

    two solutions added

--- 09-largest-number/script.js
+++ 09-largest-number/script.js
@@ -7,3 +7,20 @@ Hints:
 concat()
 parseInt()
 */
+
+function largestNumber(num) {
+    // let placeholder = '';
+
+    // for (let i = 0; i < num; i++) {
+    //     placeholder = placeholder.concat('9');
+    // }
+
+    const placeholder = '9'.repeat(num);
+
+    return parseInt(placeholder);
+}
+
+const num = 3;
+const result = largestNumber(num);
+
+console.log("result:", result);
