1ff31855e97495ae0a470bdeba2a4d3248407fbc
AIcoding-girl <51080166+AIcoding-girl@users.noreply.github.com>
2020-08-08 22:34:31 +0200

    new task added

--- 10-max-multiple/script.js
+++ 10-max-multiple/script.js
@@ -1,1 +1,11 @@
+/*
+Given a divisor and a bound, find the largest integer N such that:
+1) N is divisible by divisor;
+2) N is less than equal to bound;
+3) N is greater than 0.
 
+For example,
+for divisor = 3 and bound = 10, the output should be maxMultiple(divisor, bound) = 9;
+the largest integer divisible by 3 and not greater than 10 is 9
+
+*/
